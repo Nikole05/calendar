6b01bfe84086c11f5d66d1c25d9a0611681024f3
Nico <nico@Nicos-MacBook-Pro.local>
2018-12-12 22:46:44 +0200

    create basic react component skeleton

---
+++ react-calendar/components/Calendar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import dateFns from "date-fns";
+
+class Calendar extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>Header</div>
+        <div>Days</div>
+        <div>Cells</div>
+      </div>
+    );
+  }
+}
